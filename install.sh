ae8e7cbe1c9daeac6c3094c3188d0c8616c728a3
Elaine <elaineang95@gmail.com>
2018-10-13 07:07:08 +0000

    installation script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt install tmux
+
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+cd ~
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
