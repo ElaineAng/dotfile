37667d0abe07f2bb3cd519e49eef4f0a70dbc20b
Elaine <elaineang95@gmail.com>
2019-09-22 21:55:46 +0000

    install virtualenv

--- install.sh
+++ install.sh
@@ -9,3 +9,10 @@ cd ~
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Setup virtualenv for the jedi server.
+# Need to put the installed virtualenv in the PATH.
+sudo apt install python3-pip
+sudo pip3 install virtualenv
+
+
