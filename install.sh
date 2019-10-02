ec979074486f1f067a467d7bb613a66443c3578e
Ang <ranang2@fa19-cs523-07.cs.illinois.edu>
2019-10-02 17:24:13 -0500

    install script shouldn't use sudo

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt install tmux
 
 sudo apt install zsh
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cd ~
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
